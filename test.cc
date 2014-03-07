88ce1531a7e7291d8014c68d7bf00cfd395d77a1
Dave Cridland <dcridland@arcode.com>
2014-03-07 17:12:47 +0000

    Added simple test/example, moved into subdir

---
+++ test.cc
@@ -0,0 +1,58 @@
+#include <sigslot/sigslot.h>
+#include <iostream>
+
+class Source {
+public:
+	mutable sigslot::signal0<> signal_zero;
+	mutable sigslot::signal1<bool> signal_bool;
+	
+	Source() = default;
+	Source(Source &) = delete;
+	Source(Source &&) = delete;
+
+	void kerpling() {
+		m_toggle = !m_toggle;
+		signal_bool.emit(m_toggle);
+	}
+	
+	void boioing() const {
+		signal_zero.emit();
+	}
+private:
+	bool m_toggle{true};
+};
+
+class Sink : public sigslot::has_slots<> {
+public:
+	Sink() = default;
+	
+	void slot_bool(bool b) {
+		std::cout << "Signalled bool(" << (b ? "true" : "false" ) << ")" << std::endl;
+	}
+	
+	void slot_void() {
+		std::cout << "Signalled void." << std::endl;
+	}
+};
+
+int main() {
+	Source source;
+	
+	source.kerpling();
+	source.boioing();
+
+	{
+		Sink sink;
+
+		source.signal_zero.connect(&sink, &Sink::slot_void);
+		source.signal_bool.connect(&sink, &Sink::slot_bool);
+
+		source.kerpling();
+		source.boioing();
+	}
+	
+	source.kerpling();
+	source.boioing();
+	
+	return 0;
+}
